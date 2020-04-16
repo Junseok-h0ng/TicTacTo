03a365c4175d829305484aca8d354eb6c0e8399d
Junseok Hong <wnstjr6467@gmail.com>
2020-04-16 21:00:15 +0900

    checkBlank

--- game.js
+++ game.js
@@ -1,20 +1,39 @@
 var td = document.getElementsByTagName('td');
 var tr = document.getElementsByTagName('tr');
 
-var game = { 'lines': [], 'blanks': [] };
+var game = { 'lines': [], 'blanks': [], 'currentTurn': 'X' };
 
+//현재 누른 칸의 위치 찾기
 game.clickTd = function (event) {
-    var line = event.target.parentNode;
-    var table = line.parentNode;
-    console.log(line, table);
-    var wLine = this.lines.indexOf(line);
-
+    var wLine = this.lines.indexOf(event.target.parentNode);
+    var wBlank = this.blanks[wLine].indexOf(event.target);
+    console.log(wLine, wBlank);
+    this.checkBlank(wLine, wBlank);
 }
 
+game.checkBlank = function (wLine, wBlank) {
+    console.log('check')
+    var currentLocation = this.blanks[wLine][wBlank].textContent;
+    if (currentLocation != "") {
+
+    } else {
+        console.log('x');
+        if (this.currentTurn === 'X') {
+            this.blanks[wLine][wBlank].textContent = this.currentTurn;
+            this.currentTurn = 'O';
+        } else {
+            this.blanks[wLine][wBlank].textContent = this.currentTurn;
+            this.currentTurn = 'X';
+        }
+    }
+}
+
+//칸을 클릭
 function clickTd(event) {
     game.clickTd(event);
 }
 
+//줄들 과 칸들 배열에 넣기
 var init = function () {
     var k = -1;
     for (var i = 0; i < 3; i++) {
