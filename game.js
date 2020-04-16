098986cbddcd4e4185dcb199dae5ec9bf94e0078
Junseok Hong <wnstjr6467@gmail.com>
2020-04-16 21:27:29 +0900

    checkLine

--- game.js
+++ game.js
@@ -11,23 +11,56 @@ game.clickTd = function (event) {
     this.checkBlank(wLine, wBlank);
 }
 
+//현재 누른 칸의 칸채우기
 game.checkBlank = function (wLine, wBlank) {
-    console.log('check')
     var currentLocation = this.blanks[wLine][wBlank].textContent;
     if (currentLocation != "") {
 
     } else {
-        console.log('x');
-        if (this.currentTurn === 'X') {
-            this.blanks[wLine][wBlank].textContent = this.currentTurn;
-            this.currentTurn = 'O';
-        } else {
-            this.blanks[wLine][wBlank].textContent = this.currentTurn;
-            this.currentTurn = 'X';
-        }
+        this.blanks[wLine][wBlank].textContent = this.currentTurn;
+        this.checkLine(wLine, wBlank);
     }
+
 }
 
+//3줄의 칸이 같은지 찾기
+game.checkLine = function (wLine, wBlank) {
+    //가로줄 체크
+    if (this.blanks[wLine][0].textContent === this.currentTurn &&
+        this.blanks[wLine][1].textContent === this.currentTurn &&
+        this.blanks[wLine][2].textContent === this.currentTurn) {
+        console.log("dd");
+    }
+    //세로줄 체크
+    if (this.blanks[0][wBlank].textContent === this.currentTurn &&
+        this.blanks[1][wBlank].textContent === this.currentTurn &&
+        this.blanks[2][wBlank].textContent === this.currentTurn) {
+        console.log('rr');
+    }
+    //대각선 체크
+    if ((wLine - wBlank === 0) || (Math.abs(wLine + wBlank === 2))) {
+        if ((this.blanks[0][0].textContent === this.currentTurn &&
+            this.blanks[1][1].textContent === this.currentTurn &&
+            this.blanks[2][2].textContent === this.currentTurn)
+            || this.blanks[0][2].textContent === this.currentTurn &&
+            this.blanks[1][1].textContent === this.currentTurn &&
+            this.blanks[2][0].textContent === this.currentTurn) {
+            console.log('aa');
+        }
+    }
+
+    this.changeTurn(wLine, wBlank);
+}
+
+game.changeTurn = function (wLine, wBlank) {
+    if (this.currentTurn === 'X') {
+        this.blanks[wLine][wBlank].textContent = this.currentTurn;
+        this.currentTurn = 'O';
+    } else {
+        this.blanks[wLine][wBlank].textContent = this.currentTurn;
+        this.currentTurn = 'X';
+    }
+}
 //칸을 클릭
 function clickTd(event) {
     game.clickTd(event);
